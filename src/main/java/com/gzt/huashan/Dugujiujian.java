a91b0a7b4e19b2c6710527a110571d9e31dd6632
gzt1304518815 <1304518815@qq.com>
2020-07-11 17:03:11 +0800

    dugujiujian

---
+++ src/main/java/com/gzt/huashan/Dugujiujian.java
@@ -0,0 +1,15 @@
+package com.gzt.huashan;
+
+/**
+ * @author shkstart
+ * @create 2020-07-11 16:48
+ */
+public class Dugujiujian {
+    public static void main(String[] args) {
+        System.out.println("dugu999999999999jian");
+        System.out.println("dugu999999999999jian");
+        System.out.println("dugu999999999999jian");
+        System.out.println("dugu999999999999jian");
+        //红色表示为追踪代码 绿色表示暂存区
+    }
+}
