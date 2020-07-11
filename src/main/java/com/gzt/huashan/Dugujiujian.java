2ff2058cc6d21c93d90f8c50c4af361722f5035a
gztwlkq <gztwlkq@126.com>
2020-07-11 18:01:20 +0800

    dugujiujian5

--- src/main/java/com/gzt/huashan/Dugujiujian.java
+++ src/main/java/com/gzt/huashan/Dugujiujian.java
@@ -16,7 +16,7 @@ public class Dugujiujian {
         System.out.println("dugu999999999999jian");
         System.out.println("11111111111111111111a");
         System.out.println("11111111111111111111");
-        System.out.println("11111111111111111111");
+        System.out.println("11111111111111111111b");
         System.out.println("11111111111111111111");
         //红色 表示为追踪代码 绿色 表示未追踪代码首次进暂存区
         // 黑色 已提交本地库 蓝色 已提交本地库后 又发生了修改 且未提交
