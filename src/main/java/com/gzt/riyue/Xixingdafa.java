a9636bec71ce7fe084f19f4e68c10af61223137e
gzt1304518815 <1304518815@qq.com>
2020-07-12 15:48:22 +0800

    xixing

---
+++ src/main/java/com/gzt/riyue/Xixingdafa.java
@@ -0,0 +1,11 @@
+package com.gzt.riyue;
+
+/**
+ * @author shkstart
+ * @create 2020-07-12 15:47
+ */
+public class Xixingdafa {
+    public static void main(String[] args) {
+        System.out.println("吸星大法好");
+    }
+}
